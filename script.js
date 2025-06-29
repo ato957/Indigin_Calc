a0f508dfa60c6d1277ae47e5d9deb62fc4b3cfa2
knana4539 <knana4539@gmail.com>
2025-06-29 20:40:10 +0000

    second commit

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+function addNumber(){
+    var val1 = parseInt(document.getElementById("value1").value);
+    var val2 = parseInt(document.getElementById("value2").value);
+    var result = document.getElementById("result");
+    result.value = val1 + val2;
+}
+
+function subtractNumber(){
+    var val1 = parseInt(document.getElementById("value1").value);
+    var val2 = parseInt(document.getElementById("value2").value);
+    var result = document.getElementById("difference");
+    result.value = val1 - val2;
+}
+
+function multiplyNumber(){
+    var val1 = parseInt(document.getElementById("value1").value);
+    var val2 = parseInt(document.getElementById("value2").value);
+    var result = document.getElementById("product");
+    result.value = val1 * val2;
+}
+
+function divideNumber(){
+    var val1 = parseInt(document.getElementById("value1").value);
+    var val2 = parseInt(document.getElementById("value2").value);
+    var result = document.getElementById("quotient");
+    result.value = val1 / val2;
+}
+
 
